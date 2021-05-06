971a77123db766193f82b278d2aa6fd9ccc5f6de
cristobalcastrillon <44101193+cristobalcastrillon@users.noreply.github.com>
2021-05-06 09:45:03 -0500

    Primer commit de grafos.

---
+++ Grafos/grafo.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <list>
+using namespace std;
+// Un grafo representado con lista de adyacencia
+struct Grafo
+{
+    int V; // numero de v ́ertices
+    // Lista de adyacencia como un arreglo de v ́ertices 
+    list<int> * listaAdy;
+    Grafo(int V);   // Constructor
+    // funcion para insertar una arista al grafo
+    void insertaArista(int v, int w);
+    bool esConexo();
+};
